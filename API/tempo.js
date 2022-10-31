80385bb17a6d445def03eb0f17ce23986140e0e0
Rafael <Rafaelwvs16@gmail.com>
2022-10-31 16:27:03 -0300

    grid e iframe

--- API/tempo.js
+++ API/tempo.js
@@ -1,4 +1,18 @@
-function tempo(request, response) {
-    
+async function tempo(request, response) {
+    const dynamicDate  = new Date();
+
+    const subscribersResponse = await fetch("https://api.convertkit.com/v3/forms?api_key=<your_public_api_key>")
+
+    const subscribersResponseJson = await subscribersResponse.jason();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
+      })
+
 
 }
+
+export default tempo;
