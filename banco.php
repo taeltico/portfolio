b2f140ada9d9218fa1d32e66f1bc63309819f438
Rafael <rafaelwvs16@icloud.com>
2022-11-05 20:44:19 -0300

    php

--- banco.php
+++ banco.php
@@ -5,4 +5,12 @@ $profissao = "pizzaiolo";
 
 
 echo "meu nome é {$nome} tenho {$idade} e sou {$profissao}";
-?>
+
+ $num1 = $_GET['num1'];
+ $num2 = $_GET['num2'];
+ echo "Soma         : ","$num1+$num2 = ",$num1+$num2, "<br />";
+ echo "Subtração        : ","$num1-$num2 = ",$num1-$num2, "<br />";
+ echo "Multiplicação    : ","$num1*$num2 = ",$num1*$num2, "<br />";
+ echo "Divisão         : ","$num1/$num2 = ",$num1/$num2, "<br />";
+ echo "Resto da divisão : ","$num1%$num2 = ",$num1%$num2, "<br />";
+   ?>
