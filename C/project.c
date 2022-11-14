eb9ea1c44fab810cc58edf5c77accd100596791e
Rafael <rafaelwvs16@icloud.com>
2022-11-14 08:15:09 -0300

    contador

---
+++ C/project.c
@@ -0,0 +1,14 @@
+#include
+
+#include
+
+int main()
+{
+    int contador = 0;
+    while (contador <10) {
+        printf("valor do contador; %d\n"contador);
+        contador++;
+    }
+    return 0;
+    
+}
