a7579f1b01631bc3c3a4affb7dfb6b7b310fc4a6
Rafael <rafaelwvs16@icloud.com>
2022-11-16 18:38:09 -0300

    botao

--- C/project.c
+++ C/project.c
@@ -10,5 +10,25 @@ int main()
         contador++;
     }
     return 0;
+
+    for(contador =0;contador <10; contador++ ) {
+        printf("%d\n", contador);
+
+    }
+
+    int v1 = 10;
+
+    if(v1 != 10){
+        
+    }
+    while(v1 <= 11){
+        
+    }
+    
+    return 0;
+    return -1;
+    
+
+
     
 }
